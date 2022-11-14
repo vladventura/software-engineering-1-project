5e1c04e3c9591a1012038754a1c4ee1413310059
Vladimir Ventura <vladventura@outlook.es>
2022-11-14 12:44:45 -0500

    This component represents a single Calendar day

--- frontend/src/components/CalendarItem/index.jsx
+++ frontend/src/components/CalendarItem/index.jsx
@@ -1,3 +1,4 @@
+import { CalendarEvent } from "../CalendarEvent";
 import "./index.css";
 
 export const CalendarItem = ({ number, events = [], isToday = false }) => {
@@ -6,6 +7,11 @@ export const CalendarItem = ({ number, events = [], isToday = false }) => {
       <div className={`calendar-item-number ${isToday ? "today" : ""}`}>
         {number}
       </div>
+      <CalendarEvent
+        dueTime={events[0].dueTime}
+        name={events[0].name}
+        official={events[0].official}
+      />
     </div>
   );
 };
