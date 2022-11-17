8d3da723c899fdd3425e983e640c787348687b7f
Vladimir Ventura Furcal <vladventura@outlook.es>
2022-11-17 15:54:59 -0500

    Rendering multiple days from the mock data

--- frontend/src/components/CalendarDisplay/index.jsx
+++ frontend/src/components/CalendarDisplay/index.jsx
@@ -1,4 +1,13 @@
+import personalCalendar from "../../mocks/calendar";
+import { CalendarItem } from "../CalendarItem";
+import "./index.css";
+
 export const CalendarDisplay = () => {
-    return <div className="grid-container">
+  return (
+    <div className="grid-container">
+      {personalCalendar.days.map((d) => (
+        <CalendarItem number={d.number} isToday={false} events={d.events} />
+      ))}
     </div>
-}
+  );
+};
