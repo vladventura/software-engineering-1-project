bc541af82f5007f7f346017920b5cc9c82fff6da
Vladimir Ventura Furcal <vladventura@outlook.es>
2022-11-17 15:55:19 -0500

    Preparing to split with grid

--- frontend/src/components/MainPage/index.jsx
+++ frontend/src/components/MainPage/index.jsx
@@ -1,10 +1,21 @@
 import personalCalendar, { singleDay } from "../../mocks/calendar";
+import { CalendarDisplay } from "../CalendarDisplay";
 import { CalendarItem } from "../CalendarItem";
+import { Header } from "../Header";
+import "./index.css";
 
 const MainPage = () => {
   return (
-    <div className="main-page container">
-      <CalendarItem number={singleDay.number} events={singleDay.events} isToday={true}/>
+    <div className="main-page-container">
+      <div className="header-section">
+        <Header />
+      </div>
+      <div className="body-section">
+        <div className="body-section-sidebar"></div>
+        <div className="body-section-calendar">
+          <CalendarDisplay />
+        </div>
+      </div>
     </div>
   );
 };
