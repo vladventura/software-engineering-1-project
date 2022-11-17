99e8f170bde3a24de056cc38fa145b82d64a7efc
Vladimir Ventura Furcal <vladventura@outlook.es>
2022-11-17 16:34:23 -0500

    Refining Header

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,3 +1,10 @@
-export const Header = (
-    <div className="header-container"></div>
-);
+import logo from "../../assets/uml_logo.png";
+import "./index.css";
+
+export const Header = () => {
+  return (
+    <div className="header-container">
+      <img src={logo} alt="School Logo" className="school-logo" />
+    </div>
+  );
+};
