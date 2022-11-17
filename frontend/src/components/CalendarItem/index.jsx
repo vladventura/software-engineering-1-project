66e4608399fc9bf4af80a3be5d4f6e0fb8554396
Vladimir Ventura Furcal <vladventura@outlook.es>
2022-11-17 16:53:02 -0500

    Adding border, calcuated size

--- frontend/src/components/CalendarItem/index.jsx
+++ frontend/src/components/CalendarItem/index.jsx
@@ -7,11 +7,13 @@ export const CalendarItem = ({ number, events = [], isToday = false }) => {
       <div className={`calendar-item-number ${isToday ? "today" : ""}`}>
         {number}
       </div>
-      <CalendarEvent
-        dueTime={events[0].dueTime}
-        name={events[0].name}
-        official={events[0].official}
-      />
+      <div className="calendar-item-events-container">
+        <CalendarEvent
+          dueTime={events[0].dueTime}
+          name={events[0].name}
+          official={events[0].official}
+        />
+      </div>
     </div>
   );
 };
