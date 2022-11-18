3f89a38fca389c864aa5f315521afe379490b5d7
Vladimir Ventura <vladventura@outlook.es>
2022-11-18 12:08:40 -0500

    Fixing for gh pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import { getAll } from "./store/actions/reducerActions";
-import { BrowserRouter as Router, Route, Routes , Switch} from 'react-router-dom';
-import MainPage from './components/MainPage';
-import { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from "react-router-dom";
+import MainPage from "./components/MainPage";
+import { useEffect } from "react";
+
+const repoHomePath = "/software-engineering-1-project";
 
 const AppComponent = (props) => {
-
   useEffect(() => {
     props.getAll();
   }, []);
 
   return (
     <Routes>
-        <Route exact path="/" element={<MainPage />} />
+      <Route exact path={repoHomePath} element={<MainPage />} />
     </Routes>
   );
-}
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    getAll: () => dispatch(getAll())
+    getAll: () => dispatch(getAll()),
   };
 };
 
