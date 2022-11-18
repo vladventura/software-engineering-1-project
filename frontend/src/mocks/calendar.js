057cc332bd843db306a1b6784b399e5ce17367a0
Vladimir Ventura <vladventura@outlook.es>
2022-11-18 03:02:34 -0500

    Changing map for forEach and linting

--- frontend/src/mocks/calendar.js
+++ frontend/src/mocks/calendar.js
@@ -2,36 +2,36 @@
 let events = [];
 let days = [];
 let personalCalendar = {
-    color: '#facfad',
-    days: [],
-    name: 'Personal Calendar',
+  color: "#facfad",
+  days: [],
+  name: "Personal Calendar",
 };
 
 for (let x = 1; x < 32; x++) {
-    let day = {
-        date: `11/${x}/2022`,
-        month: '11',
-        year: '2022',
-        number: x,
-        events: []
-    };
-    days.push(day);
+  let day = {
+    date: `11/${x}/2022`,
+    month: "11",
+    year: "2022",
+    number: x,
+    events: [],
+  };
+  days.push(day);
 
-    let event = {
-        date: `11/${x}/2022`,
-        number: x,
-        dueTime: '1:00pm',
-        description: `An event ${x}`,
-        official: x % 2 == 0,
-        calendar: '',
-        name: `Event${x}`,
-        color: personalCalendar.color,
-    };
-    events.push(event);
+  let event = {
+    date: `11/${x}/2022`,
+    number: x,
+    dueTime: "1:00pm",
+    description: `An event ${x}`,
+    official: x % 2 == 0,
+    calendar: "",
+    name: `Event${x}`,
+    color: personalCalendar.color,
+  };
+  events.push(event);
 }
 
-days.map(day => {
-    day.events = events.filter(e => e.date === day.date);
+days.forEach((day) => {
+  day.events = events.filter((e) => e.date === day.date);
 });
 
 personalCalendar.days = days;
