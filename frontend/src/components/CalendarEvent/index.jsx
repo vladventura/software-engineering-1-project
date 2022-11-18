b749012314c05adcfd303dd23aa12e4d3ac3fc92
Vladimir Ventura <vladventura@outlook.es>
2022-11-18 02:46:38 -0500

    Effort to improve events looks

--- frontend/src/components/CalendarEvent/index.jsx
+++ frontend/src/components/CalendarEvent/index.jsx
@@ -1,7 +1,9 @@
 import "./index.css";
 
-export const CalendarEvent = ({ dueTime, name, official }) => <div className="calendar-event">
+export const CalendarEvent = ({ dueTime, name, official }) => (
+  <div className="calendar-event">
     <p className="calendar-event-due">{dueTime}</p>
     <p className="calendar-event-name">{name}</p>
-    {official && <div className="calendar-event-official"></div>}
-</div>;
+    {official && <p className="calendar-event-official">✔</p>}
+  </div>
+);
