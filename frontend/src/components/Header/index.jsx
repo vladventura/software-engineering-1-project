23d286bd4f548c9e56fa2a660d2646c8bf6cc284
Vladimir Ventura <vladventura@outlook.es>
2022-11-18 02:38:25 -0500

    Adding target blank with noreferrer, old browser good practice

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -4,7 +4,14 @@ import "./index.css";
 export const Header = () => {
   return (
     <div className="header-container">
-      <img src={logo} alt="School Logo" className="school-logo" />
+      <a
+        className="school-logo-link"
+        href="https://uml.edu"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <img src={logo} alt="School Logo" className="school-logo" />
+      </a>
     </div>
   );
 };
