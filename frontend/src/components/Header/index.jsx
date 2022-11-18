08f33af96ee70a16738f9da5c9f6574a55e44ff7
Vladimir Ventura <vladventura@outlook.es>
2022-11-18 03:27:40 -0500

    Efforts for stubbing the header

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -3,24 +3,37 @@ import avatar from "../../assets/avatar.png";
 import "./index.css";
 
 export const Header = () => {
+  const avatarOnClick = () => {
+    alert("Please make-believe that this is a proper user status pop-up");
+  };
+
+  const logoOnClick = () => {
+    window.open("https://uml.edu", "_blank", "noreferrer");
+  };
+
   return (
     <div className="header-container">
-      <a
-        className="school-logo-link"
-        href="https://uml.edu"
-        rel="noreferrer"
-        target="_blank"
-      >
-        <img src={logo} alt="School Logo" className="school-logo" />
-      </a>
-      <a
-        className="avatar-link"
-        href="https://uml.edu"
-        rel="noreferrer"
-        target="_blank"
-      >
-        <img src={avatar} alt="User Avatar" className="avatar-picture" />
-      </a>
+      <img
+        src={logo}
+        alt="School Logo"
+        className="school-logo clickable-img"
+        onClick={logoOnClick}
+      />
+      <div className="avatar-container">
+        {/* <a
+          className="avatar-link"
+          href="https://uml.edu"
+          rel="noreferrer"
+          target="_blank"
+        > */}
+        <img
+          src={avatar}
+          alt="User Avatar"
+          className="avatar-picture clickable-img"
+          onClick={avatarOnClick}
+        />
+        <div className="student-name">Student Name</div>
+      </div>
     </div>
   );
 };
