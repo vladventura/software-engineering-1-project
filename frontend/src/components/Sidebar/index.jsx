4d68e2dc418d54d5c8b23a691ce5bdadcf155487
Vladimir Ventura <vladventura@outlook.es>
2022-11-18 05:11:58 -0500

    Adding stubs for create event, notifications, and calendars onto the sidebar

--- frontend/src/components/Sidebar/index.jsx
+++ frontend/src/components/Sidebar/index.jsx
@@ -1,3 +1,44 @@
-export const Sidebar = () => (
-  <div className="sidebar-container">Just a sidebar here nothing to see</div>
-);
+import "./index.css";
+
+export const Sidebar = () => {
+  const createCalendarOnClick = () => {
+    alert("Please make-believe this is a create calendar pop-up");
+  };
+
+  const createEventOnClick = () => {
+    alert("Please make-believe this is a create event pop-up");
+  };
+
+  const createNotificationOnClick = () => {
+    alert("Please make-beleive this is a create notification pop-up");
+  };
+
+  return (
+    <div className="sidebar-container">
+      <div className="new-calendar-container">
+        <button className="new-calendar-button" onClick={createCalendarOnClick}>
+          Create Calendar
+        </button>
+      </div>
+      <div className="calendar-preview-container">
+        <div className="calendar-preview-frame">
+          <p>Calendar Preview</p>
+        </div>
+      </div>
+      <div className="calendars-toggle-container">
+        <div className="calendar-toggle">A personal calendar</div>
+      </div>
+      <div className="sidebar-buttons-container">
+        <button className="add-event-button" onClick={createEventOnClick}>
+          Create Event
+        </button>
+        <button
+          className="add-notification-button"
+          onClick={createNotificationOnClick}
+        >
+          Create Notification
+        </button>
+      </div>
+    </div>
+  );
+};
