60937d7246a8c11f59c94d05a2e8485b2ff01235
Vladimir Ventura <vladventura@outlook.es>
2022-11-21 12:34:54 -0500

    Preparing a calendars reducer:

---
+++ frontend/src/store/reducers/calendarsReducer.js
@@ -0,0 +1,10 @@
+import personalCalendar from "../../mocks/calendar";
+
+const initState = {
+  allCalendars: [personalCalendar],
+};
+
+export const calendarsReducer = (state = initState, action) => {
+  switch (action.type) {
+  }
+};
