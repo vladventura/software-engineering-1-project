07d06722f6b98a503ab9ddc9d2a88e21802311e1
Vladimir Ventura <vladventura@outlook.es>
2022-12-03 11:33:45 -0500

    Changing the mock data to better reflect our needs

--- frontend/src/mocks/calendar.js
+++ frontend/src/mocks/calendar.js
@@ -3,40 +3,28 @@ let events = [];
 let days = [];
 let personalCalendar = {
   color: "#facfad",
-  days: [],
   name: "Personal Calendar",
+  events: [],
   visible: true,
 };
 
 for (let x = 1; x < 32; x++) {
-  let day = {
-    date: `11/${x}/2022`,
-    month: "11",
-    year: "2022",
-    number: x,
-    events: [],
-  };
-  days.push(day);
-
   let event = {
-    date: `11/${x}/2022`,
+    date: `12/${x}/2022`,
     number: x,
+    month: 12,
     dueTime: "1:00pm",
     description: `An event ${x}`,
     official: x % 2 === 0,
-    calendar: "",
+    calendar: personalCalendar.name,
     name: `Event${x}`,
     color: personalCalendar.color,
   };
   events.push(event);
 }
 
-days.forEach((day) => {
-  day.events = events.filter((e) => e.date === day.date);
-});
-
-personalCalendar.days = days;
+personalCalendar.events = events;
 
 export default personalCalendar;
-export const singleEvent = days[0].events[0];
+export const singleEvent = events[0];
 export const singleDay = days[0];
