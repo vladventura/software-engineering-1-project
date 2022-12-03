af392d1f3893983effd60db6f4e28c65ca2ba3a8
Vladimir Ventura <vladventura@outlook.es>
2022-12-03 11:34:23 -0500

    Adding composer for redux devtools and removing strict mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,30 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 // TODO: Migrate to Redux TK
-import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
 import { BrowserRouter } from "react-router-dom";
 import thunk from "redux-thunk";
 import { rootReducer } from "./store";
 import App from "./App";
 import "./index.css";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composed = compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const store = createStore(rootReducer, composed);
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
