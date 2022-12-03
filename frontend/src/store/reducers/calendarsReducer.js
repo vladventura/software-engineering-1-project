6dabc34397dcfe52c9635ad737086638f1fd357c
Vladimir Ventura <vladventura@outlook.es>
2022-12-03 11:35:27 -0500

    Adding more actions to the reducer

--- frontend/src/store/reducers/calendarsReducer.js
+++ frontend/src/store/reducers/calendarsReducer.js
@@ -1,10 +1,41 @@
-import personalCalendar from "../../mocks/calendar";
+import {
+  GET_ALL_CALENDARS,
+  GET_INITIAL_INFO,
+  TOGGLE_CALENDAR,
+} from "../actions/actionTypes";
 
 const initState = {
-  allCalendars: [personalCalendar],
+  allCalendars: [],
+  selectedDate: 0,
+  selectedMonth: 0,
+  selectedYear: 0,
+  selectedDay: 0,
+  daysInMonth: 0,
 };
 
 export const calendarsReducer = (state = initState, action) => {
   switch (action.type) {
+    case GET_ALL_CALENDARS:
+      return {
+        ...state,
+        allCalendars: action.payload,
+      };
+    case TOGGLE_CALENDAR:
+      // Not too good
+      let changedVis = [...state.allCalendars];
+      changedVis.forEach((c) => {
+        if (c.name === action.payload) c.visible = !c.visible;
+      });
+      return {
+        ...state,
+        allCalendars: changedVis,
+      };
+    case GET_INITIAL_INFO:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    default:
+      return state;
   }
 };
