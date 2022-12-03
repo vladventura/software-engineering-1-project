4830a327b1ad49ab95513c044abe6da09b6f0ea4
Vladimir Ventura <vladventura@outlook.es>
2022-12-03 15:39:50 -0500

    Removing unused stuff, adding modal data from store

--- frontend/src/components/MainPage/index.jsx
+++ frontend/src/components/MainPage/index.jsx
@@ -1,28 +1,28 @@
 import "./index.css";
-import { useState } from "react";
 import { CalendarDisplay } from "../CalendarDisplay";
 import { Header } from "../Header";
 import { Sidebar } from "../Sidebar";
+import { connect } from "react-redux";
 
-const MainPage = () => {
-  const [viewC, toggleViewC] = useState(true);
-  const toggleCalendar = (name) => {
-    toggleViewC(!viewC);
-  };
-
+const MainPage = ({ modal }) => {
+  console.log(modal);
   return (
     <div className="main-page-container">
       <div className="header-section">
         <Header />
       </div>
       <div className="body-section-sidebar">
-        <Sidebar toggleViewCalendar={toggleCalendar} viewCalendar={viewC} />
+        <Sidebar />
       </div>
       <div className="body-section-calendar">
-        <CalendarDisplay viewCal={viewC} />
+        <CalendarDisplay />
       </div>
     </div>
   );
 };
 
-export default MainPage;
+const stateToProps = (state) => ({
+  modal: state.modal,
+});
+
+export default connect(stateToProps)(MainPage);
