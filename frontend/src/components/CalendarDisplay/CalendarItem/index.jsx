396b750491a53786e5d46aaf1a708c4e80cb7aa8
Vladimir Ventura <vladventura@outlook.es>
2022-12-03 11:39:22 -0500

    Moving the CalendarItem and renaming accordingly

---
+++ frontend/src/components/CalendarDisplay/CalendarItem/index.jsx
@@ -0,0 +1,36 @@
+import { CalendarEvent } from "./CalendarEvent";
+import "./index.css";
+
+export const CalendarItem = ({
+  number,
+  events = [],
+  isToday = false,
+  month,
+}) => {
+  const itemOnDblClick = () => {
+    alert(
+      "Please make-believe this is an Add Event dialog for now on day " +
+        number +
+        " of month " +
+        month
+    );
+  };
+  return (
+    <div className="calendar-item" onDoubleClick={itemOnDblClick}>
+      <div className={`calendar-item-number ${isToday ? "today" : ""}`}>
+        {number}
+      </div>
+      <div className="calendar-item-events-container">
+        {events.map((e) => (
+          <CalendarEvent
+            dueTime={e.dueTime}
+            name={e.name}
+            official={e.official}
+            description={e.description}
+            key={`event-${e.name}-${e.number}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
