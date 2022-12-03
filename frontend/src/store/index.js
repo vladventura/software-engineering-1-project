01132cbe3dcae2fa4b4b0bd7f2bd54bf08ff631c
Vladimir Ventura <vladventura@outlook.es>
2022-12-03 15:37:56 -0500

    Adding modal reducer to the store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { calendarsReducer } from "./reducers/calendarsReducer";
+import { modalReducer } from "./reducers/modalReducer";
 
 export const rootReducer = combineReducers({
   calendars: calendarsReducer,
+  modal: modalReducer,
 });
