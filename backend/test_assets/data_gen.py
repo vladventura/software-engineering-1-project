93022aaf16cb0829feea05e85697a33361b5678a
Jae Choi <jaec1989@gmail.com>
2022-12-03 15:14:25 -0500

    Added changes to dictionary definitions for the data sets.

--- backend/test_assets/data_gen.py
+++ backend/test_assets/data_gen.py
@@ -1,28 +1,57 @@
 import json
 
-events1 = {
-    "homework1": {
+# example of a class dictionary
+# Each items are titled json object containing a dictionary defining the event
+c1_dict = {
+    "Homework1": {
         "title": "Homework1",
         "description": "Dummy Homework",
-        "date": "some date",
+        "date": "2022-12-6",
+        "duration": "some duration",
+        "repeats": False,
+        "frequency": 3,
+    },
+    "Quiz1": {
+        "title": "Quiz1",
+        "description": "Dummy Quiz Event",
+        "date": "2022-12-10",
         "duration": "some duration",
         "repeats": False,
         "frequency": 3,
     }
 }
 
-classes = {
-    "class1": events1
+# example of a calendar json object. events is a json object containing dictionary of events
+class1 = {
+    "title": "class1",
+    "events": c1_dict,
+    "is_class": True
+}
+
+p_dict = {
+    "Christmas!": {
+        "title": "Christmas!",
+        "description": "Special Holiday!",
+        "date": "2022-12-25",
+        "duration": "some duration",
+        "repeats": False,
+        "frequency": 0,
+    }
 }
 
 # We should probably fill this one out as this is supposed to be the customizable one
-personal = {
-
+personal1 = {
+    "title": "Personal1",
+    "events": p_dict,
+    "is_class": False
 }
 
+
+# Structure - Dictionary that can be searched by the keyword title.
+# Dictionary containing calendars where each calendar is a dictionary
 calendars = {
-    "classes": classes,
-    "personal": personal
+    class1["title"]: class1,
+    personal1["title"]: personal1
 }
 
 
