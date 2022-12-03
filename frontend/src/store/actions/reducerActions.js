1a6c7d62aab739a64fb929eefe1ed67a8c8c0e7d
Vladimir Ventura <vladventura@outlook.es>
2022-12-03 14:56:27 -0500

    Using month names

--- frontend/src/store/actions/reducerActions.js
+++ frontend/src/store/actions/reducerActions.js
@@ -5,6 +5,21 @@ import {
   TOGGLE_CALENDAR,
 } from "./actionTypes";
 
+const monthNames = {
+  1: "January",
+  2: "February",
+  3: "March",
+  4: "April",
+  5: "May",
+  6: "June",
+  7: "July",
+  8: "August",
+  9: "September",
+  10: "October",
+  11: "November",
+  12: "December",
+};
+
 export const getAllCalendars = () => {
   return (dispatch, getState) => {
     // return axios....
@@ -45,6 +60,7 @@ export const getInitialInfo = () => {
           selectedYear,
           daysInMonth,
           selectedDay,
+          monthName: monthNames[selectedMonth],
         },
       })
     );
