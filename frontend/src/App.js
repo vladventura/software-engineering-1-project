d4228b2709f8ea2a0a75a66080965ab577bbe7c4
Vladimir Ventura <vladventura@outlook.es>
2022-12-03 11:35:01 -0500

    Adding the fetch actions at App level

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+import { connect } from "react-redux";
+import { useEffect } from "react";
+import {
+  getAllCalendars,
+  getInitialInfo,
+} from "./store/actions/reducerActions";
 import MainPage from "./components/MainPage";
 
 // Do not change, this is for deploying on github pages
 const repoHomePath = "/software-engineering-1-project";
 
-const AppComponent = () => {
+const AppComponent = ({ getAll, getInitial }) => {
+  useEffect(() => {
+    getAll();
+    getInitial();
+  }, [getAll, getInitial]);
+
   return (
     <Routes>
       <Route exact path={repoHomePath} element={<MainPage />} />
@@ -12,4 +23,9 @@ const AppComponent = () => {
   );
 };
 
-export default AppComponent;
+const dispatchToProps = (dispatch) => ({
+  getAll: () => dispatch(getAllCalendars()),
+  getInitial: () => dispatch(getInitialInfo()),
+});
+
+export default connect(null, dispatchToProps)(AppComponent);
