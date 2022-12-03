c89e9b5c60b62fffa47a761ffac5ba61a665a41c
Vladimir Ventura <vladventura@outlook.es>
2022-12-03 17:44:06 -0500

    Fixing nontoggle {kludge}

--- frontend/src/components/SidebarToggleCalendar/index.jsx
+++ frontend/src/components/SidebarToggleCalendar/index.jsx
@@ -1,27 +1,30 @@
+import { useState } from "react";
 import { connect } from "react-redux";
 import { toggleCalendar } from "../../store/actions/calendarActions";
 import "./index.css";
 
 const SidebarToggleCalendarComponent = ({ calendar, toggleViewCalendar }) => {
-  const { name, color, visible } = calendar;
+  const [visible, setVisible] = useState(calendar.visible);
+  const { name, color } = calendar;
 
   const changeChecked = () => {
+    // KLUDGE: Ideally we don't need this
+    // but I'm running out of time to debug
+    setVisible(!visible);
     toggleViewCalendar(name);
   };
 
   return (
-    <div
-      className="calendar-toggle"
-      onClick={changeChecked}
-      style={{ backgroundColor: color }}
-    >
+    <div className="calendar-toggle" style={{ backgroundColor: color }}>
       <input
         className="calendar-toggle-checkmark"
         checked={visible}
         onChange={changeChecked}
         type="checkbox"
       />
-      <div className="calendar-toggle-name">{name}</div>
+      <div className="calendar-toggle-name" onClick={changeChecked}>
+        {name}
+      </div>
     </div>
   );
 };
