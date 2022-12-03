dcec5dd2ee86aa7e589446a9cd342fb3a68f4470
Vladimir Ventura <vladventura@outlook.es>
2022-12-03 11:38:37 -0500

    Getting calendard from store, then sorting the visible events from them, then making a map of their events, then generating day items for each day in the given month

--- frontend/src/components/CalendarDisplay/index.jsx
+++ frontend/src/components/CalendarDisplay/index.jsx
@@ -1,20 +1,51 @@
-import personalCalendar from "../../mocks/calendar";
-import { CalendarDay } from "./CalendarDay";
 import "./index.css";
+import { CalendarItem } from "./CalendarItem";
+import { connect } from "react-redux";
 
-export const CalendarDisplay = ({ viewCal }) => {
-  // Calendar should filter
+const CalendarDisplayComponent = ({
+  allCalendars,
+  daysInMonth,
+  selectedMonth,
+  selectedDay,
+}) => {
+  const viewableEvents = [];
+  allCalendars.forEach((c) => {
+    if (c.visible) {
+      c.events.forEach((e) => {
+        if (e.month === selectedMonth) viewableEvents.push(e);
+      });
+    }
+  });
+
+  const eventsPerDay = {};
+  viewableEvents.map((e) =>
+    eventsPerDay[e.number]
+      ? eventsPerDay[e.number].push(e)
+      : (eventsPerDay[e.number] = [e])
+  );
+
   return (
     <div className="grid-container">
-      {personalCalendar.days.map((d) => (
-        <CalendarDay
-          number={d.number}
-          isToday={false}
-          events={d.events}
-          month={d.month}
-          showEvents={viewCal}
+      {[...Array(daysInMonth)].map((d, i) => (
+        <CalendarItem
+          number={i + 1}
+          key={`calendar-${i + 1}-day`}
+          events={eventsPerDay[i + 1]}
+          month={selectedMonth}
+          isToday={selectedDay === i + 1}
         />
       ))}
     </div>
   );
 };
+
+const stateToProps = (state) => {
+  return {
+    allCalendars: state.calendars.allCalendars,
+    daysInMonth: state.calendars.daysInMonth,
+    selectedMonth: state.calendars.selectedMonth,
+    selectedDay: state.calendars.selectedDay,
+  };
+};
+
+export const CalendarDisplay = connect(stateToProps)(CalendarDisplayComponent);
