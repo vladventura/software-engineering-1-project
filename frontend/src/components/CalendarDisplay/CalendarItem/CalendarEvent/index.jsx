f3509796cf0792a5e1448d588c3143997297aa79
Vladimir Ventura <vladventura@outlook.es>
2022-12-03 13:40:14 -0500

    Using the event instead. Coloring events by their parent calendar color

--- frontend/src/components/CalendarDisplay/CalendarItem/CalendarEvent/index.jsx
+++ frontend/src/components/CalendarDisplay/CalendarItem/CalendarEvent/index.jsx
@@ -1,11 +1,16 @@
 import "./index.css";
 
-export const CalendarEvent = ({ dueTime, name, official, description }) => {
+export const CalendarEvent = ({ event }) => {
+  const { description, name, dueTime, official, color } = event;
   const eventOnClick = () => {
     alert("Event Info " + description);
   };
   return (
-    <div className="calendar-event" onClick={eventOnClick}>
+    <div
+      className="calendar-event"
+      onClick={eventOnClick}
+      style={{ backgroundColor: color }}
+    >
       <p className="calendar-event-due">{dueTime}</p>
       <p className="calendar-event-name">{name}</p>
       {official && <p className="calendar-event-official">📓</p>}
