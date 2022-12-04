5f7b78ebf38ecd366b71e5cc5b631f1b926e9856
Vladimir Ventura <vladventura@outlook.es>
2022-12-04 11:40:09 -0500

    Adding the notification modal to the screen

--- frontend/src/components/Modal/index.jsx
+++ frontend/src/components/Modal/index.jsx
@@ -1,23 +1,17 @@
 // Credit https://github.com/machadop1407/React-Modal-Tutorial
 
 import React from "react";
-import { connect } from "react-redux";
-import { closeModal } from "../../store/actions/modalActions";
 import { AddEventModal } from "./AddEventModal";
+import { AddNotificationModal } from "./AddNotificationModal";
 import { CreateCalendarModal } from "./CreateCalendarModal";
 import "./index.css";
 
-const ModalComponent = ({ type, close }) => {
+export const Modal = ({ type }) => {
   let modalInner = <></>;
 
   if (type === "create") modalInner = <CreateCalendarModal />;
   if (type === "addevent") modalInner = <AddEventModal />;
+  if (type === "addnotif") modalInner = <AddNotificationModal />;
 
   return <div className="modalBackground">{modalInner}</div>;
 };
-
-const dispatchToProps = (dispatch) => ({
-  close: () => dispatch(closeModal()),
-});
-
-export const Modal = connect(null, dispatchToProps)(ModalComponent);
