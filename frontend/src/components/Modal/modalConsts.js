68eefb0ef91cb40db68018ec1b1589a0d8732eae
Vladimir Ventura <vladventura@outlook.es>
2022-12-04 10:10:47 -0500

    This will be used on the add notification dialog as well

---
+++ frontend/src/components/Modal/modalConsts.js
@@ -0,0 +1,11 @@
+export const notificationTimepair = [
+  { time: 15, measure: "min" },
+  { time: 30, measure: "min" },
+  { time: 45, measure: "min" },
+  { time: 1, measure: "hr" },
+  { time: 3, measure: "hr" },
+  { time: 5, measure: "hr" },
+  { time: 1, measure: "day" },
+];
+
+export const notifMethodList = ["SMS", "Email", "Both"];
