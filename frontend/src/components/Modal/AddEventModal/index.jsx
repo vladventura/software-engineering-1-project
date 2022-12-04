ca3afe5d7827cc2edb7894aab3770fa519613c6d
Vladimir Ventura <vladventura@outlook.es>
2022-12-03 22:52:21 -0500

    Finished add event modal. Added background for time picker

--- frontend/src/components/Modal/AddEventModal/index.jsx
+++ frontend/src/components/Modal/AddEventModal/index.jsx
@@ -2,16 +2,48 @@ import "./index.css";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 import { connect } from "react-redux";
-import { createCalendar } from "../../../store/actions/calendarActions";
 import { closeModal } from "../../../store/actions/modalActions";
 import DatePicker from "react-datepicker";
+import TimePicker from "react-time-picker";
+import { createEvent } from "../../../store/actions/calendarActions";
 
-export const AddEventModalComponent = ({ close, create }) => {
+export const AddEventModalComponent = ({
+  close,
+  allCalendars,
+  targetDate,
+  create,
+}) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(
+    targetDate ? new Date(targetDate) : new Date()
+  );
+  const [time, setTime] = useState("10:00");
+  const [calendarIndex, setCalendarIndex] = useState(0);
 
-  const submit = () => {};
+  const dropdownChange = (e) => {
+    setCalendarIndex(e.target.value);
+  };
+
+  const submit = () => {
+    const targetCalendar = allCalendars[calendarIndex];
+    const { color } = targetCalendar;
+    const number = date.getDate();
+    const month = date.getMonth() + 1;
+    const dateString = `${month}/${number}/${date.getFullYear()}`;
+
+    const event = {
+      name,
+      date: dateString,
+      number,
+      month,
+      dueTime: time,
+      description,
+      calendar: targetCalendar.name,
+      color,
+    };
+    create(event);
+  };
 
   return (
     <div className="modalContainer">
@@ -44,7 +76,26 @@ export const AddEventModalComponent = ({ close, create }) => {
             onChange={(e) => setDescription(e.target.value)}
           />
           <label className="text">Event Date</label>
-          <DatePicker selected={date} onChange={(d) => setDate(d)} />
+          <DatePicker
+            selected={date}
+            onChange={setDate}
+            dateFormat="MM/dd/yyyy"
+          />
+          <label className="text">Event Time</label>
+          <TimePicker
+            value={time}
+            onChange={setTime}
+            className="time-picker"
+            openClockOnFocus={false}
+          />
+          <label className="text">Event Calendar</label>
+          <select id="event-chosen-calendar" onChange={dropdownChange}>
+            {allCalendars.map((n, i) => (
+              <option key={`event-chosen-option-${n.name}-${i}`} value={i}>
+                {n.name}
+              </option>
+            ))}
+          </select>
         </form>
       </div>
       <div className="footer">
@@ -57,12 +108,16 @@ export const AddEventModalComponent = ({ close, create }) => {
   );
 };
 
+const stateToProps = (state) => ({
+  allCalendars: state.calendars.allCalendars,
+});
+
 const dispatchToProps = (dispatch) => ({
   close: () => dispatch(closeModal()),
-  create: (name, color) => dispatch(createCalendar(name, color)),
+  create: (evnt) => dispatch(createEvent(evnt)),
 });
 
 export const AddEventModal = connect(
-  null,
+  stateToProps,
   dispatchToProps
 )(AddEventModalComponent);
