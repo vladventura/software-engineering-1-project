06a9b0d4a45f48756136baf80db1e921563674eb
Vladimir Ventura <vladventura@outlook.es>
2022-12-03 21:27:34 -0500

    Adding action resolver for opening add event modal

--- frontend/src/store/actions/modalActions.js
+++ frontend/src/store/actions/modalActions.js
@@ -1,25 +1,42 @@
-import { CLOSE_MODAL, OPEN_CREATE_MODAL } from "./actionTypes";
+import {
+  CLOSE_MODAL,
+  OPEN_ADD_EVENT_MODAL,
+  OPEN_CREATE_MODAL,
+} from "./actionTypes";
 
 export const openCreateModal = () => {
   return (dispatch, getState) => {
     const { modal } = getState();
     if (!modal.isOpen)
-      return new Promise(() => {
+      return new Promise(() =>
         dispatch({
           type: OPEN_CREATE_MODAL,
           payload: "create",
-        });
-      });
+        })
+      );
+  };
+};
+
+export const openAddEventModal = () => {
+  return (dispatch, getState) => {
+    const { modal } = getState();
+    if (!modal.isOpen)
+      return new Promise(() =>
+        dispatch({
+          type: OPEN_ADD_EVENT_MODAL,
+          payload: "addevent",
+        })
+      );
   };
 };
 
 export const closeModal = () => {
   return (dispatch, getState) => {
-    return new Promise(() => {
+    return new Promise(() =>
       dispatch({
         type: CLOSE_MODAL,
         payload: false,
-      });
-    });
+      })
+    );
   };
 };
