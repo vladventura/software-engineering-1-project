dcb940f809bc7313fb5102a006f3451352708816
Vladimir Ventura <vladventura@outlook.es>
2022-12-04 17:10:32 -0500

    Small stylistic changes, also added the new edit modal as a type

--- frontend/src/components/Modal/index.jsx
+++ frontend/src/components/Modal/index.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { AddEventModal } from "./AddEventModal";
 import { AddNotificationModal } from "./AddNotificationModal";
 import { CreateCalendarModal } from "./CreateCalendarModal";
+import { EditEventModal } from "./EditEventModal";
 import "./index.css";
 
 export const Modal = ({ type }) => {
@@ -12,6 +13,7 @@ export const Modal = ({ type }) => {
   if (type === "create") modalInner = <CreateCalendarModal />;
   if (type === "addevent") modalInner = <AddEventModal />;
   if (type === "addnotif") modalInner = <AddNotificationModal />;
+  if (type === "editevent") modalInner = <EditEventModal />;
 
   return <div className="modalBackground">{modalInner}</div>;
 };
