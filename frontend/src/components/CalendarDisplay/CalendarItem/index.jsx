2b495c6e1ea851ec4f15f1eca77cd1da83eecda5
Vladimir Ventura <vladventura@outlook.es>
2022-12-05 15:53:45 -0500

    Adding pagination functionality

--- frontend/src/components/CalendarDisplay/CalendarItem/index.jsx
+++ frontend/src/components/CalendarDisplay/CalendarItem/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { openAddEventModal } from "../../../store/actions/modalActions";
 import { CalendarEvent } from "./CalendarEvent";
@@ -14,17 +15,84 @@ const CalendarItemComponent = ({
     openAddEvent(dateString);
   };
 
+  const [shouldPaginate, setShouldPaginate] = useState(false);
+  const [paginatedEvents, setPaginatedEvents] = useState([]);
+  // If it works, it works
+  const [currentPage, setCurrentPage] = useState(events);
+  const [pageIndex, setCurrentPageIndex] = useState(0);
+
+  useEffect(() => {
+    if (events.length > 2) setShouldPaginate(true);
+  }, [events]);
+
+  useEffect(() => {
+    if (shouldPaginate) {
+      console.log("Should Page");
+      const paginated = events.reduce((acc, _, index, evs) => {
+        if (index % 2 === 0) acc.push(evs.slice(index, index + 2));
+        return acc;
+      }, []);
+      setPaginatedEvents(paginated);
+      setCurrentPage(paginated[0]);
+      return;
+    }
+  }, [shouldPaginate, events]);
+
+  const shouldShowRightArrow = () => pageIndex < paginatedEvents.length - 1;
+  const shouldShowLeftArrow = () => pageIndex > 0;
+
+  const pageUp = () => {
+    if (shouldShowRightArrow()) {
+      setCurrentPage(paginatedEvents[pageIndex + 1]);
+
+      setCurrentPageIndex(pageIndex + 1);
+    }
+  };
+
+  const pageDown = () => {
+    if (shouldShowLeftArrow()) {
+      setCurrentPage(paginatedEvents[pageIndex - 1]);
+      setCurrentPageIndex(pageIndex - 1);
+    }
+  };
+
+  console.log(pageIndex, shouldShowLeftArrow());
+
   return (
-    <div
-      className={`calendar-item ${isToday ? "today" : ""}`}
-      onDoubleClick={itemOnDblClick}
-    >
-      <div className="calendar-item-number">{number}</div>
-      <div className="calendar-item-events-container">
-        {events.map((e) => (
+    <div className={`calendar-item ${isToday ? "today" : ""}`}>
+      <div className="calendar-item-number" onDoubleClick={itemOnDblClick}>
+        {number}
+      </div>
+      <div
+        className="calendar-item-events-container"
+        onDoubleClick={itemOnDblClick}
+      >
+        {currentPage.map((e) => (
           <CalendarEvent event={e} key={`event-${e.name}-${e.calendar}`} />
         ))}
       </div>
+      {shouldPaginate && (
+        <div className="calendar-item-pagination-arrows">
+          <div
+            className="pagination-arrow"
+            onClick={pageDown}
+            style={{
+              visibility: shouldShowLeftArrow() ? "visible" : "hidden",
+            }}
+          >
+            {"<"}
+          </div>
+          <div
+            className="pagination-arrow"
+            onClick={pageUp}
+            style={{
+              visibility: shouldShowRightArrow() ? "visible" : "hidden",
+            }}
+          >
+            {">"}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
