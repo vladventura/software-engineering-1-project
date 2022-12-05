7f95e1e853c14e3acd7b80457a92126f10d34a89
Vladimir Ventura <vladventura@outlook.es>
2022-12-05 18:25:32 -0500

    Emergency patch: removing devtools, error when using with compose. Passing undefined doesn't stop redux from applying it

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 // TODO: Migrate to Redux TK
-import {
-  legacy_createStore as createStore,
-  applyMiddleware,
-  compose,
-} from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { BrowserRouter } from "react-router-dom";
 import thunk from "redux-thunk";
 import { rootReducer } from "./store";
 import App from "./App";
 import "./index.css";
 
-const composed = compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-const store = createStore(rootReducer, composed);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 const container = document.getElementById("root");
 const root = createRoot(container);
 
