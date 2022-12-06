b20255a3494ab31f8960650bb2717e6698f22951
Vladimir Ventura <vladventura@outlook.es>
2022-12-05 19:44:18 -0500

    Removing console log

--- frontend/src/mocks/calendar.js
+++ frontend/src/mocks/calendar.js
@@ -55,8 +55,6 @@ for (let x = 1; x < 32; x++) {
 personalCalendar.events = events;
 gui1.events = gui1Events;
 
-console.log(gui1);
-
 export default personalCalendar;
 export const guiCalendar = gui1;
 export const singleEvent = events[0];
